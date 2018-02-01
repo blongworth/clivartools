afe68616a056852a35a91d32b7700a8dc1954aeb
Brett Longworth <blongworth@gmail.com>
2018-02-01 16:15:11 -0500

    Add plot to vignette, fix plotting.

--- R/plots.R
+++ R/plots.R
@@ -7,27 +7,26 @@
 #' @import ggplot2
 # function to plot profile from dataframe returned by getProfile
 plotProfile <- function(profile) {
-    profile %>% dplyr::mutate(depth = ifelse(depth_corr == -99, depth, depth_corr)) %>%
-  ggplot(aes(depth, f_modern, shape = wheel, color = wheel)) +
+  p <- dplyr::mutate(profile, depth = ifelse(depth_corr == -99, depth, depth_corr))
+  ggplot(p, aes(depth, f_modern, shape = wheel, color = wheel)) +
     geom_errorbar(aes(ymin=f_modern-f_ext_error,
                       ymax=f_modern+f_ext_error)) + geom_line() +
     geom_point(size=2) + #facet_grid(station ~ .) +
     coord_flip() + scale_x_reverse()
 }
 
-#' Plot a depth profile from a station
+#' Plot CLIVAR data from a wheel
 #'
-#' @param profile A datatable in the format of getProfile output
+#' @param wheel A wheel name in format: "USAMS010101".
 #'
 #' @return A ggplot object
 #' @export
 #' @import ggplot2
 # function to plot profile from dataframe returned by getProfile
-plotWheelProfile <- function(profile) {
-    profile %>% dplyr::mutate(depth = ifelse(depth_corr == -99, depth, depth_corr)) %>%
-  ggplot(aes(depth, f_modern)) +
-    geom_errorbar(aes(ymin=f_modern-f_ext_error,
-                      ymax=f_modern+f_ext_error)) + geom_line() +
-    geom_point(size=2) + facet_grid(station ~ .) +
-    coord_flip() + scale_x_reverse()
+plotWheelProfile <- function(wheel) {
+
+  # get profile data for a wheel
+
+  #plot using plotProfile
+  clivartools::plotProfile(profile)
 }
